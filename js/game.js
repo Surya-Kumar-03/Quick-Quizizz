9cda1a5f23e31bf38e16b4ed5f7e619f0f60ce36
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2022-08-25 13:40:23 +0530

    Fix for EndPage

--- js/game.js
+++ js/game.js
@@ -59,7 +59,7 @@ function startGame() {
 function getNewQuestion() {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
-    return window.location.assign("end.html");
+    return window.location.replace("end.html");
   }
   questionCounter++;
   //Change qn no in html
